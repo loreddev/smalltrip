d8e91bfb84f6408025148df0ef43fbc21513bc4a
Gustavo "Guz" L de Mello <contact@guz.one>
2025-08-11 17:37:03 -0300

    chore(flake): dev shell setup via nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = {nixpkgs, ...}: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system: let
+        pkgs = import nixpkgs {inherit system;};
+      in
+        f {
+          inherit (pkgs) lib stdenv;
+          inherit pkgs;
+        });
+  in {
+    devShells = forAllSystems ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        CGO_ENABLED = "0";
+        hardeningDisable = ["all"];
+
+        buildInputs = with pkgs; [
+          # Go tools
+          go
+          golangci-lint
+          gofumpt
+          gotools
+          delve
+        ];
+      };
+    });
+  };
+}
